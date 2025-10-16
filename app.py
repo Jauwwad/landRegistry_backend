d86a5c31e4198669b80b04249c078d238d40821a
Jauwwad <jauwwad.nallamandu786@gmail.com>
2025-10-16 12:12:33 +0530

    final244

--- app.py
+++ app.py
@@ -120,4 +120,5 @@ def api_info():
 
 if __name__ == '__main__':
     app = create_app()
-    app.run(debug=True, port=5001)
+    port = int(os.environ.get('PORT', 5001))
+    app.run(host='0.0.0.0', port=port, debug=os.environ.get('FLASK_ENV') != 'production')
